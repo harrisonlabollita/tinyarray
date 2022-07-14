1d23c5941983d1398666697ba4953d6272e0ab02
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2022-07-14 07:54:30 -0700

    tests and readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # tinyarray
 
-Kind of like numpy arrays but a bit more lightweight
+Lightweight arrays for Python. 
+
+Kind of like numpy arrays but a bit more lightweight (and not as much capability). No C backend.
+
+## Installation
+```shell
+git clone https://github.com/harrisonlabollita/tinyarray.git
+cd tinyarray
+pip install .
+```
+
+## Running tests
+
+```shell
+	python -m unittest
+```
+
--- test/test_array.py
+++ test/test_array.py
@@ -13,6 +13,23 @@ class TestTinyArray(unittest.TestCase):
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
     
+    def test_sub(self):
+        x = ta.array([1,2,3,4])
+        y = ta.array([1,2,3,4])
+        z = x - y
+        t1 = z.numpy()
+        x -= y
+        t2 = x.numpy()
+        np.testing.assert_allclose(t1, t2)
+    
+    def test_div(self):
+        x = ta.array([1,2,3,4])
+        y = x*2
+        t1 = y.numpy()
+        x *= 2
+        t2 = x.numpy()
+        np.testing.assert_allclose(t1, t2)
+    
     def test_mul(self):
         x = ta.array([1,2,3,4])
         y = x*2
@@ -20,6 +37,11 @@ class TestTinyArray(unittest.TestCase):
         x *= 2
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
+        z = x*x
+        t3 = z.numpy()
+        a = x**2
+        t4 = a.numpy()
+        np.testing.assert_allclose(t3, t4)
 
     def test_sum(self):
         x = ta.array([1,2,3,4])
