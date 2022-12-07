8e9e96a775f95fb97e8978e3d305ca674496745a
harrisonlabollita <harrisonlabollita@users.noreply.github.com>
2022-12-07 01:12:39 +0000

    autopep8 action fixes

--- test/test_array.py
+++ test/test_array.py
@@ -3,52 +3,53 @@ from tinyarray.array import array
 import unittest
 import random
 
+
 class TestTinyArray(unittest.TestCase):
 
     def test_add(self):
-        x = array([1,2,3,4])
-        y = array([1,2,3,4])
+        x = array([1, 2, 3, 4])
+        y = array([1, 2, 3, 4])
         z = x + y
         t1 = z.numpy()
         x += y
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
-    
+
     def test_sub(self):
-        x = array([1,2,3,4])
-        y = array([1,2,3,4])
+        x = array([1, 2, 3, 4])
+        y = array([1, 2, 3, 4])
         z = x - y
         t1 = z.numpy()
         x -= y
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
-    
+
     def test_div(self):
-        x = array([1,2,3,4])
-        y = x/2
+        x = array([1, 2, 3, 4])
+        y = x / 2
         t1 = y.numpy()
         x /= 2
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
-    
+
     def test_mul(self):
-        x = array([1,2,3,4])
-        y = x*2
+        x = array([1, 2, 3, 4])
+        y = x * 2
         t1 = y.numpy()
         x *= 2
         t2 = x.numpy()
         np.testing.assert_allclose(t1, t2)
-        z = x*x
+        z = x * x
         t3 = z.numpy()
         a = x**2
         t4 = a.numpy()
         np.testing.assert_allclose(t3, t4)
 
     def test_sum(self):
-        x = array([1,2,3,4])
+        x = array([1, 2, 3, 4])
         ans = x.sum()
         self.assertEqual(ans, 10)
-        x = array([1.,2.,3.,4.])
+        x = array([1., 2., 3., 4.])
         ans = x.sum()
         self.assertEqual(ans, 10.)
 
@@ -61,47 +62,47 @@ class TestTinyArray(unittest.TestCase):
         random.seed(1234)
         x = array.uniform(10).sort()
 
-        t1 =  array([0.007491470058587191, 
-                   0.08393822683708396, 
-                   0.23680977536311776, 
-                   0.4407325991753527, 
-                   0.5822275730589491, 
-                   0.6715634814879851, 
-                   0.7664809327917963, 
-                   0.9109759624491242, 
-                   0.939268997363764, 
-                   0.9664535356921388]).numpy()
+        t1 = array([0.007491470058587191,
+                    0.08393822683708396,
+                    0.23680977536311776,
+                    0.4407325991753527,
+                    0.5822275730589491,
+                    0.6715634814879851,
+                    0.7664809327917963,
+                    0.9109759624491242,
+                    0.939268997363764,
+                    0.9664535356921388]).numpy()
         y = x.reverse().numpy()
-        np.testing.assert_allclose(y,t1[::-1])
+        np.testing.assert_allclose(y, t1[::-1])
 
     def test_prod(self):
-        x =  array([1, 3, 5, 7])
+        x = array([1, 3, 5, 7])
         ans = x.prod()
         self.assertEqual(ans, 105)
 
     def test_sort(self):
         random.seed(1234)
-        x =  array.uniform(10)
+        x = array.uniform(10)
         x1 = x.sort().numpy()
         x2 = x.argsort().numpy()
-        t1 =  array([0.007491470058587191, 
-                   0.08393822683708396, 
-                   0.23680977536311776, 
-                   0.4407325991753527, 
-                   0.5822275730589491, 
-                   0.6715634814879851, 
-                   0.7664809327917963, 
-                   0.9109759624491242, 
-                   0.939268997363764, 
-                   0.9664535356921388]).numpy()
-        t2 =  array([2, 7, 9, 1, 5, 6, 8, 3, 4, 0]).numpy()
+        t1 = array([0.007491470058587191,
+                    0.08393822683708396,
+                    0.23680977536311776,
+                    0.4407325991753527,
+                    0.5822275730589491,
+                    0.6715634814879851,
+                    0.7664809327917963,
+                    0.9109759624491242,
+                    0.939268997363764,
+                    0.9664535356921388]).numpy()
+        t2 = array([2, 7, 9, 1, 5, 6, 8, 3, 4, 0]).numpy()
         np.testing.assert_allclose(x1, t1)
         np.testing.assert_allclose(x2, t2)
 
     def test_complex(self):
         random.seed(1234)
         x = array.uniform(10)
-        y = array.uniform(10)*1j
+        y = array.uniform(10) * 1j
         z = x + y
         t1 = z.numpy()
         x += y
@@ -111,19 +112,19 @@ class TestTinyArray(unittest.TestCase):
     def test_sqrt_cpx(self):
         random.seed(1234)
         x = array.uniform(10)
-        y = array.uniform(10)*1j
+        y = array.uniform(10) * 1j
         z = x + y
-        
+
         t1 = z.sqrt().numpy()
         t2 = np.sqrt(z.numpy())
         np.testing.assert_allclose(t1, t2)
-        
+
     def test_exp_cpx(self):
         random.seed(1234)
         x = array.uniform(10)
-        y = array.uniform(10)*1j
+        y = array.uniform(10) * 1j
         z = x + y
-        
+
         t1 = z.exp().numpy()
         t2 = np.exp(z.numpy())
         np.testing.assert_allclose(t1, t2)
@@ -131,53 +132,53 @@ class TestTinyArray(unittest.TestCase):
     def test_log_cpx(self):
         random.seed(1234)
         x = array.uniform(10)
-        y = array.uniform(10)*1j
+        y = array.uniform(10) * 1j
         z = x + y
         t1 = z.log().numpy()
         t2 = np.log(z.numpy())
         np.testing.assert_allclose(t1, t2)
 
-
     def test_find(self):
         random.seed(1234)
-        x =  array.uniform(10)
-        t1 = x.find(0.5, func=lambda x, val: x >=val).numpy()
-        t2 =  array([0.9664535356921388, 0.9109759624491242, 
-                    0.939268997363764, 0.5822275730589491, 
+        x = array.uniform(10)
+        t1 = x.find(0.5, func=lambda x, val: x >= val).numpy()
+        t2 = array([0.9664535356921388, 0.9109759624491242,
+                    0.939268997363764, 0.5822275730589491,
                     0.6715634814879851, 0.7664809327917963]).numpy()
         np.testing.assert_allclose(t1, t2)
 
         t1 = x.find(0.5, func=lambda x, val: x < val).numpy()
-        t2 =  array([0.4407325991753527, 0.007491470058587191, 0.08393822683708396, 0.23680977536311776]).numpy()
+        t2 = array([0.4407325991753527, 0.007491470058587191,
+                    0.08393822683708396, 0.23680977536311776]).numpy()
         np.testing.assert_allclose(t1, t2)
-        
 
     def test_getitem(self):
         random.seed(1234)
-        x =  array.uniform(10)
-        t1 = x[x>=0.5].numpy()
-        t2 = x.find(0.5, func=lambda x, val: x >=val).numpy()
+        x = array.uniform(10)
+        t1 = x[x >= 0.5].numpy()
+        t2 = x.find(0.5, func=lambda x, val: x >= val).numpy()
         np.testing.assert_allclose(t1, t2)
 
-        x =  array.uniform(10)
-        t1 = x[x<0.5].numpy()
+        x = array.uniform(10)
+        t1 = x[x < 0.5].numpy()
         t2 = x.find(0.5, func=lambda x, val: x < val).numpy()
         np.testing.assert_allclose(t1, t2)
 
     def test_ops(self):
         random.seed(1234)
-        x =  array.uniform(10)
+        x = array.uniform(10)
         t = x.numpy()
-        np.testing.assert_allclose(np.sqrt(t), 
+        np.testing.assert_allclose(np.sqrt(t),
                                    x.sqrt().numpy())
-        np.testing.assert_allclose(np.log(t), 
+        np.testing.assert_allclose(np.log(t),
                                    x.log().numpy())
-        np.testing.assert_allclose(np.exp(t), 
+        np.testing.assert_allclose(np.exp(t),
                                    x.exp().numpy())
-        np.testing.assert_allclose(np.min(t), 
+        np.testing.assert_allclose(np.min(t),
                                    x.min())
-        np.testing.assert_allclose(np.max(t), 
+        np.testing.assert_allclose(np.max(t),
                                    x.max())
+
 
 if __name__ == "__main__":
     unittest.main()
--- tinyarray/array.py
+++ tinyarray/array.py
@@ -15,23 +15,26 @@ class array:
 
     def __iter__(self: array): return self._data.__iter__()
 
-    def __getitem__(self: array, idx: Union[slice, int, bool]) -> Union[float, int, complex]:
+    def __getitem__(
+            self: array, idx: Union[slice, int, bool]) -> Union[float, int, complex]:
         if isinstance(idx, int):
             idx = idx if idx >= 0 else idx + self._shape
             return self._data[idx]
         elif isinstance(idx, slice):
-            return array([self._data[i] for i in range(*idx.indices(len(self._data)))])
+            return array([self._data[i]
+                          for i in range(*idx.indices(len(self._data)))])
         elif hasattr(idx, "__iter__"):  # <- should this be better?
             if isinstance(idx[0], bool):
-                return array([self._data[i] for i, ind in enumerate(idx) if ind])
+                return array([self._data[i]
+                              for i, ind in enumerate(idx) if ind])
             elif isinstance(idx[0], int):
                 return array([self._data[i] for i in idx])
         else:
             raise NotImplementedError(
                 "This type of indexing has not been implemented")
 
-
-    def __setitem__(self: array, idx: Union[slice, int, bool], value: Union[float, int, complex]) -> None:
+    def __setitem__(
+            self: array, idx: Union[slice, int, bool], value: Union[float, int, complex]) -> None:
         if isinstance(idx, int):
             idx = idx if idx >= 0 else idx + self._shape
             self._data[idx] = value
@@ -55,139 +58,168 @@ class array:
 
     def __str__(self: array, /) -> str: return self._data.__str__()
 
-    def __add__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __add__(self: array, other: Union[int, float, complex,
+                                          List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] + other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] + other[i] for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] + other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] + other[i] for i in range(len(self._data))])
+            return array([self._data[i] + other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
 
-
-    def __iadd__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __iadd__(self: array, other: Union[int, float, complex,
+                                           List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] + other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
 
-            return array([self._data[i] + other[i] for i in range(len(self._data))])
+            return array([self._data[i] + other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] + other[i] for i in range(len(self._data))])
+            return array([self._data[i] + other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
 
     __radd__ = __add__
 
-    def __sub__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __sub__(self: array, other: Union[int, float, complex,
+                                          List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] - other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] - other[i] for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] - other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] - other[i]  for i in range(len(self._data))])
+            return array([self._data[i] - other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
     __rsub__ = __sub__
 
-    def __isub__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __isub__(self: array, other: Union[int, float, complex,
+                                           List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] - other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] - other[i] for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] - other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] - other[i] for i in range(len(self._data))])
+            return array([self._data[i] - other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
 
-
-    def __mul__(self: array, other: Union[int, float, complex,List[Union[int, float, complex]]], /) -> array:
+    def __mul__(self: array, other: Union[int, float, complex,
+                                          List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] * other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * other[i] for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] * other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * other[i] for i in range(len(self._data))])
+            return array([self._data[i] * other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
-    
+
     __rmul__ = __mul__
 
-    def __imul__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __imul__(self: array, other: Union[int, float, complex,
+                                           List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
             res = [self._data[i] * other
                    for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * other[i] for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] * other[i]
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * other[i] for i in range(len(self._data))])
+            return array([self._data[i] * other[i]
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
 
-
-    def __truediv__(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
+    def __truediv__(
+            self: array, other: Union[int, float, complex, List[Union[int, float, complex]]], /) -> array:
         if isinstance(other, (int, float, complex)):
-            res = [self._data[i] * 1/(other) for i in range(len(self._data))]
+            res = [self._data[i] * 1 / (other) for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * 1/(other[i]) for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] * 1 / (other[i])
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * 1/(other[i]) for i in range(len(self._data))])
+            return array([self._data[i] * 1 / (other[i])
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
 
     __rtruediv__ = __truediv__
 
-    def __itruediv__(self: array, other: Union[int, float, List[Union[int, float]]], /) -> array:
+    def __itruediv__(
+            self: array, other: Union[int, float, List[Union[int, float]]], /) -> array:
         if isinstance(other, (int, float, complex)):
-            res = [self._data[i] * 1/(other) for i in range(len(self._data))]
+            res = [self._data[i] * 1 / (other) for i in range(len(self._data))]
             return array(res)
         elif isinstance(other, array):
-            assert len(other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * 1/(other[i]) for i in range(len(self._data))])
+            assert len(
+                other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
+            return array([self._data[i] * 1 / (other[i])
+                          for i in range(len(self._data))])
         elif isinstance(other, List):
             assert len(
                 other) == self._shape, f"incompatible shapes {len(other)} != {self._shape}"
-            return array([self._data[i] * 1/(other[i]) for i in range(len(self._data))])
+            return array([self._data[i] * 1 / (other[i])
+                          for i in range(len(self._data))])
         else:
             raise NotImplementedError(
                 f"Type {type(other)} is not implemented!")
@@ -211,7 +243,7 @@ class array:
         return array(res)
 
     def __le__(self: array, other: Union[int, float, complex]) -> array:
-        if not isinstance(other, complex) and sel.dtype !=complex:
+        if not isinstance(other, complex) and sel.dtype != complex:
             res = [a <= other for a in self._data]
         else:
             res = [a.real <= other.real for a in self._data]
@@ -227,7 +259,8 @@ class array:
     def __eq__(self: array, other: Union[int, float, complex]) -> array:
         return array([a == other for a in self._data])
 
-    def append(self: array, other: Union[int, float, complex, List[Union[int, float, complex]]]) -> None:
+    def append(self: array, other: Union[int, float,
+                                         complex, List[Union[int, float, complex]]]) -> None:
         if isinstance(other, (int, float, complex)):
             self._data.append(other)
         elif hasattr(other, "__iter__"):
@@ -237,20 +270,24 @@ class array:
             raise Exception(f"{other} can not be appended to an array object")
 
     # Removing shape allows for tinyarray's arrays to be "plottable" by matplotlib
-    #@property
+    # @property
     #def shape(self: array) -> tuple: return tuple([self._shape])
 
     @property
-    def dtype(self: array) -> Union[float, int, complex]: return type(self._data[0])
+    def dtype(self: array) -> Union[float, int,
+                                    complex]: return type(self._data[0])
 
     @property
-    def data(self: array) -> List[Union[float, int, complex]]: return self._data
-    
-    @property
-    def real(self: array) -> List[Union[float, int]]: return array([a.real for a in self._data])
+    def data(self: array) -> List[Union[float,
+                                        int, complex]]: return self._data
 
     @property
-    def imag(self: array) -> List[Union[float, int]]: return array([a.imag for a in self._data])
+    def real(self: array) -> List[Union[float, int]
+                                  ]: return array([a.real for a in self._data])
+
+    @property
+    def imag(self: array) -> List[Union[float, int]
+                                  ]: return array([a.imag for a in self._data])
 
     def numpy(self: array) -> np.ndarray: return np.array(self._data)
 
@@ -262,13 +299,15 @@ class array:
             self._data.extend([0 for _ in range(N)])
             self._shape = len(self._data)
         elif self.dtype == complex:
-            self._data.extend([0.0+0.0j for _ in range(N)])
+            self._data.extend([0.0 + 0.0j for _ in range(N)])
             self._shape = len(self._data)
 
-    def argsort(self: array) -> array: 
-        return array(sorted(range(len(self._data)), key=self._data.__getitem__))
+    def argsort(self: array) -> array:
+        return array(sorted(range(len(self._data)),
+                            key=self._data.__getitem__))
 
-    def sort(self: array) -> array: return array(sorted(self._data, key=lambda x: x.real))
+    def sort(
+        self: array) -> array: return array(sorted(self._data, key=lambda x: x.real))
 
     def find(self: array, val, func=lambda x, val: x ==
              val) -> array: return array([a for a in self._data if func(a, val)])
@@ -276,30 +315,31 @@ class array:
     def sum(self: array) -> Union[float, int]: return sum(self._data)
 
     def mean(self: array) -> Union[float, complex,
-                                   int]: return sum(self._data)/self._shape
-    
+                                   int]: return sum(self._data) / self._shape
+
     def arg(self: array) -> array:
-        return array([math.atan(a.imag/a.real) for a in self._data])
+        return array([math.atan(a.imag / a.real) for a in self._data])
 
     def modulus(self: array) -> array:
         return array([math.sqrt(a.real**2 + a.imag**2) for a in self._data])
 
-    def sqrt(self: array) -> array: 
+    def sqrt(self: array) -> array:
         if self.dtype in (int, float):
             return array([math.sqrt(a) for a in self._data])
         elif self.dtype == complex:
             def cpx_sqrt(z):
                 norm_z = math.sqrt(z.real**2 + z.imag**2)
-                r = math.sqrt((norm_z + z.real)/2)
-                i = (z.imag/abs(z.imag))*math.sqrt((norm_z - z.real)/2)
-                return r + i*1j
+                r = math.sqrt((norm_z + z.real) / 2)
+                i = (z.imag / abs(z.imag)) * math.sqrt((norm_z - z.real) / 2)
+                return r + i * 1j
             return array(list(map(cpx_sqrt, self._data)))
-    
-    def exp(self: array) -> array: 
+
+    def exp(self: array) -> array:
         if self.dtype in (int, float):
             return array([math.exp(a) for a in self._data])
         else:
-            return array( [math.exp(a.real)*(math.cos(a.imag) + 1j*math.sin(a.imag)) for a in self._data])
+            return array([math.exp(a.real) * (math.cos(a.imag) +
+                                              1j * math.sin(a.imag)) for a in self._data])
 
     def log(self: array) -> array:
         if self.dtype in (int, float):
@@ -307,25 +347,30 @@ class array:
         else:
             m = self.modulus()
             argz = self.arg()
-            return array([math.log(m[i]) + 1j*argz[i] for i in range(len(m))])
+            return array([math.log(m[i]) + 1j * argz[i]
+                          for i in range(len(m))])
 
-    def dot(self: array, other) -> Union[float, int, complex]: return sum(self*other)
+    def dot(self: array, other) -> Union[float,
+                                         int, complex]: return sum(self * other)
+
     def max(self: array) -> Union[float, int, complex]: return max(self._data)
     def min(self: array) -> Union[float, int, complex]: return min(self._data)
     def reverse(self: array) -> array: return array(self._data[::-1])
-    def conjugate(self: array) -> array: return array([a.conjugate() for a in self._data])
+
+    def conjugate(
+        self: array) -> array: return array([a.conjugate() for a in self._data])
 
     def prod(self: array) -> Union[float, int, complex]:
         res = self._data[0]
         for i in range(1, len(self)):
             res *= self._data[i]
         return res
-    
+
     @classmethod
     def ones(cls: array, N: int) -> array: return cls([1 for _ in range(N)])
     @classmethod
     def zeros(cls: array, N: int) -> array: return cls([0 for _ in range(N)])
 
     @classmethod
-    def uniform(cls: array, N: int, a: Union[int,float] = 0.0,
-                b: Union[int,float] = 1.0) -> array: return cls([random.uniform(a, b) for _ in range(N)])
+    def uniform(cls: array, N: int, a: Union[int, float] = 0.0,
+                b: Union[int, float] = 1.0) -> array: return cls([random.uniform(a, b) for _ in range(N)])
